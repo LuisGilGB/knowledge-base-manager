dcabe9d5610bd7fd81b2d5b6c00885c18607358f
LuisGilGB <luis.gilgb@gmail.com>
2025-04-05 11:56:44 +0200

    Handling the fetching of connections with Suspense

--- src/components/ConnectionsSidebar.tsx
+++ src/components/ConnectionsSidebar.tsx
@@ -1,12 +1,62 @@
 'use client';
 
 import { useConnections } from "@/lib/api/hooks";
-import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarGroup, SidebarGroupContent, SidebarGroupLabel } from "./ui/sidebar";
 import Link from "next/link";
+import ViewBoundary from "./boundaries/ViewBoundary";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { Skeleton } from "./ui/skeleton";
+import { CircleAlert } from "lucide-react";
+
+const ConnectionsGroupContentWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarGroupContent className="flex flex-col gap-2">
+      <SidebarMenu>
+        {children}
+      </SidebarMenu>
+    </SidebarGroupContent>
+  );
+};
+
+const ConnectionsGroupContent = () => {
+  const { data: connections } = useConnections({ suspense: true });
+
+  return (
+    <ConnectionsGroupContentWrapper>
+      {connections?.map((connection) => (
+        <SidebarMenuItem key={connection.connection_id}>
+          <Link href={`/${connection.connection_id}`}>
+            <SidebarMenuButton tooltip={connection.name} className="data-[slot=sidebar-menu-button]:!p-1.5">
+              <span>{connection.name}</span>
+            </SidebarMenuButton>
+          </Link>
+        </SidebarMenuItem>
+      ))}
+    </ConnectionsGroupContentWrapper>
+  );
+};
+
+const ConnectionsGroupContentErrorFallback = () => {
+  return (
+    <ConnectionsGroupContentWrapper>
+      <div className="flex flex-col items-center justify-center gap-2 my-4">
+        <CircleAlert className="size-6 text-red-400" />
+        <p className="text-red-400 text-center">Connections could not be loaded</p>
+      </div>
+    </ConnectionsGroupContentWrapper>
+  );
+}
+
+const ConnectionsGroupContentFallback = () => {
+  return (
+    <ConnectionsGroupContentWrapper>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <Skeleton key={index} className="w-full h-8 bg-primary/10" />
+      ))}
+    </ConnectionsGroupContentWrapper>
+  );
+};
 
 const ConnectionsSidebar = () => {
-  const { data: connections } = useConnections();
-
   return (
     <Sidebar>
       <SidebarHeader>
@@ -26,19 +76,12 @@ const ConnectionsSidebar = () => {
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel className="text-base font-semibold mb-2"><h2>Connections</h2></SidebarGroupLabel>
-          <SidebarGroupContent className="flex flex-col gap-2">
-            <SidebarMenu>
-              {connections?.map((connection) => (
-                <SidebarMenuItem key={connection.connection_id}>
-                  <Link href={`/${connection.connection_id}`}>
-                    <SidebarMenuButton tooltip={connection.name} className="data-[slot=sidebar-menu-button]:!p-1.5">
-                      <span>{connection.name}</span>
-                    </SidebarMenuButton>
-                  </Link>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+          <ViewBoundary
+            fallback={<ConnectionsGroupContentFallback />}
+            errorFallback={<ConnectionsGroupContentErrorFallback />}
+          >
+            <ConnectionsGroupContent />
+          </ViewBoundary>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
